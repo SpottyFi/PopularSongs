6f0fc7aeaa3b462560d1af69b6763d6b380f2559
Vikram Raja <vikramraja1995@users.noreply.github.com>
2018-09-28 11:08:23 -0700

    Create mongo import script

---
+++ seed/mongo_export.sh
@@ -0,0 +1,4 @@
+mongoimport -d albumlist-musicplayer -c artists --jsonArray --file ../../seed_data.json && 
+mongoimport -d albumlist-musicplayer -c artists --jsonArray --file ../../seed_data1.json &&
+mongoimport -d albumlist-musicplayer -c artists --jsonArray --file ../../seed_data2.json &&
+mongoimport -d albumlist-musicplayer -c artists --jsonArray --file ../../seed_data3.json

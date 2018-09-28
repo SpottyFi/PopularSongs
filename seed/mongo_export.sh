d9bcffbc5be9d5e2c356c5e553c3908757f695e3
Vikram Raja <vikramraja1995@users.noreply.github.com>
2018-09-28 12:31:43 -0700

    Update mongodb import script

--- seed/mongo_export.sh
+++ seed/mongo_export.sh
@@ -1,4 +1,5 @@
-mongoimport -d albumlist-musicplayer -c artists --jsonArray --file ../../seed_data.json && 
-mongoimport -d albumlist-musicplayer -c artists --jsonArray --file ../../seed_data1.json &&
-mongoimport -d albumlist-musicplayer -c artists --jsonArray --file ../../seed_data2.json &&
-mongoimport -d albumlist-musicplayer -c artists --jsonArray --file ../../seed_data3.json
+ls &&
+mongoimport -d albumlist-musicplayer -c artists --jsonArray --file ./data/seed_data.json && 
+mongoimport -d albumlist-musicplayer -c artists --jsonArray --file ./data/seed_data1.json &&
+mongoimport -d albumlist-musicplayer -c artists --jsonArray --file ./data/seed_data2.json &&
+mongoimport -d albumlist-musicplayer -c artists --jsonArray --file ./data/seed_data3.json

98ee71823b76e17072745845a5e67715966d9fcb
Vikram Raja <vikramraja1995@users.noreply.github.com>
2018-09-28 15:48:21 -0700

    Optimize seed scripts and update mongo imports

--- seed/mongo_export.sh
+++ seed/mongo_export.sh
@@ -1,5 +1,4 @@
-ls &&
-mongoimport -d albumlist-musicplayer -c artists --jsonArray --file ./data/seed_data.json && 
-mongoimport -d albumlist-musicplayer -c artists --jsonArray --file ./data/seed_data1.json &&
+mongoimport -d albumlist-musicplayer -c artists --jsonArray --file ./data/seed_data1.json && 
 mongoimport -d albumlist-musicplayer -c artists --jsonArray --file ./data/seed_data2.json &&
-mongoimport -d albumlist-musicplayer -c artists --jsonArray --file ./data/seed_data3.json
+mongoimport -d albumlist-musicplayer -c artists --jsonArray --file ./data/seed_data3.json &&
+mongoimport -d albumlist-musicplayer -c artists --jsonArray --file ./data/seed_data4.json

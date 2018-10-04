db64cf637820a69d42c5af59396cb4be75d70ca3
Vikram Raja <vikramraja1995@users.noreply.github.com>
2018-10-03 19:06:16 -0700

    Rename cassandra to scylla

--- seed/scylla.sh
+++ seed/scylla.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 SEED_DIR=$(pwd)
-CQL_FILE=$SEED_DIR/cassandra.cql
+CQL_FILE=$SEED_DIR/scylla.cql
 SEED_FILE=$SEED_DIR/data/nosql/artists.csv
 
 printf '\e[33mDefining Database and Tables...\e[0m\n'
@@ -11,3 +11,6 @@ printf '\n\e[33mCopying Data...\e[0m\n'
 cqlsh -e "COPY spottyfi.artists(artist_id, album_id, song_id, artist_name, album_name, album_image, published_year, song_name, song_streams, song_length, song_popularity) FROM '${SEED_FILE}' WITH HEADER=TRUE AND DELIMITER='|';"
 
 printf '\n\e[32mImport Complete!\e[0m'
+
+# Optional command using cassandra-loader if cqlsh is buggy
+# ./cassandra-loader -f artists.csv -host 172.17.0.2 -schema "spottyfi.artists(artist_id, album_id, song_id, artist_name, album_name, album_image, published_year, song_name, song_streams, song_length, song_popularity)" -delim "|"

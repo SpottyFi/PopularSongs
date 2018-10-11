557ac941de7029ec26cee42460406a886f2b9de4
Vikram Raja <vikramraja1995@users.noreply.github.com>
2018-10-10 17:18:03 -0700

    Fix all server endpoints

--- server/worker.js
+++ server/worker.js
@@ -7,65 +7,45 @@ const db = require('../database/cassandra');
 
 const app = express();
 
-// parse application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({ extended: false }));
-// parse application/json
 app.use(bodyParser.json());
 app.use(cors());
 app.use(express.static(path.join(__dirname, '../public/')));
 
-app.get('/artist/:artistId/:albumId', (req, res) => {
+app.get('/artist/:artistId', (req, res) => {
   const artistId = parseInt(req.params.artistId, 10);
-  const albumId = parseInt(req.params.albumId, 10);
-  db.getTopTen(artistId, albumId).then(test => {
-    res.send(test);
+  db.getTopTen(artistId).then(songs => {
+    res.send(songs);
   });
-  // mongodb
-  //   .getSongs(artistId)
-  //   .then(songs => res.send(songs))
-  //   .catch(err => {
-  //     console.error(err);
-  //     res.sendStatus(400);
-  //   });
 });
 
-// expect to receive {artistID, albumID, songID, added -> bool either 1 or 0}
-// app.post('/artist/', (req, res) => {
-//   let update = {};
-//   var objProp = `albums.${req.body.albumID}.songs`;
-//   update[objProp] = !!parseInt(req.body.added, 10);
+app.post('/artist/', (req, res) => {
+  db.addSong(req.body)
+    .then(() => res.sendStatus(201))
+    .catch(() => res.sendStatus(500));
+});
 
-//   Artists.findOneAndUpdate({ id: req.body.artistID }, { $set: update })
-//     // TO DO: get current boolean value from db and send back along with mssg
-//     .then(() => res.json({ message: 'success', added: !!parseInt(req.body.added, 10) }))
-//     .catch(() => res.status(400).json({ message: 'bad request' }));
-// });
+app.put('/artist/update', (req, res) => {
+  if (req.body.albumID === undefined || req.body.artistID === undefined) {
+    res.sendStatus(400);
+  }
+  db.updateSong(req.body)
+    .then(() => res.sendStatus(200))
+    .catch(() => res.sendStatus(500));
+});
 
-// app.put('/artist/update', (req, res) => {
-//   if (req.body.albumID === undefined || req.body.songID === undefined) {
-//     res.sendStatus(400);
-//   }
-//   mongodb
-//     .updateSong(req.body)
-//     .then(result => res.json(result))
-//     .catch(err => {
-//       console.error(err);
-//       res.sendStatus(400);
-//     });
-// });
-
-// app.delete('/artist/delete', (req, res) => {
-//   if (req.body.albumID === undefined || req.body.songID === undefined) {
-//     res.sendStatus(400);
-//   }
-//   mongodb
-//     .updateSong(req.body)
-//     .then(() => res.json({ message: 'success' }))
-//     .catch(err => {
-//       console.error(err);
-//       res.sendStatus(400);
-//     });
-// });
+app.delete('/artist/delete', (req, res) => {
+  if (
+    req.body.albumID === undefined ||
+    req.body.artistID === undefined ||
+    req.body.songName === undefined
+  ) {
+    res.sendStatus(400);
+  }
+  db.deleteSong(req.body)
+    .then(() => res.sendStatus(200))
+    .catch(() => res.sendStatus(500));
+});
 
 const PORT = 3000;
 

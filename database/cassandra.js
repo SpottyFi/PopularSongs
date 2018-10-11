618e00f06a2ddd158c667cf236bbe4fa19fecbd2
Vikram Raja <vikramraja1995@users.noreply.github.com>
2018-10-10 17:24:36 -0700

    Fix getTopTen function

--- database/cassandra.js
+++ database/cassandra.js
@@ -22,7 +22,45 @@ models.setDirectory(path.join(__dirname, '/models')).bind(
   }
 );
 
-const getTopTen = (artistId, albumId) =>
-  models.instance.Artists.findAsync({ artist_id: artistId, album_id: albumId });
+const getTopTen = artistId => {
+  return models.instance.Artists.findAsync({ artist_id: artistId })
+    .then(data => {
+      return data.sort((song1, song2) => {
+        // Sorted in descending order
+        if (song1.song_popularity === song2.song_popularity) {
+          if (song1.song_streams > song2.song_streams) {
+            return -1;
+          }
+          if (song1.song_streams < song2.song_streams) {
+            return 1;
+          }
+          return 0;
+        }
+        if (song1.song_popularity > song2.song_popularity) {
+          return -1;
+        }
+        if (song1.song_popularity < song2.song_popularity) {
+          return 1;
+        }
+      });
+    })
+    .then(data => {
+      const songs = [];
+      for (let i = 0; i < 10; i += 1) {
+        const songData = data[i];
+        const song = {};
+        song.albumID = songData.album_id;
+        song.artistID = songData.artist_id;
+        song.songID = songData.song_id;
+        song.popularity = songData.song_popularity;
+        song.streams = songData.song_streams;
+        song.albumURL = songData.album_image.replace(/[']/g, '');
+        song.albumName = songData.album_name.replace(/[']/g, '');
+        song.songName = songData.song_name.replace(/[']/g, '');
+        songs.push(song);
+      }
+      return songs;
+    });
+};
 
 module.exports.getTopTen = getTopTen;
